aea1557a1f0015048b60f5ca2e191c19db7f42c4
AnastasisGi <anastasiosgks@gmail.com>
2020-08-08 11:18:51 +0100

    Update with Ellen

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -5,15 +5,23 @@ class DockingStation
   def initialize()
     @bikes = []
   end
+
   def dock(bike)
+    return  "Sorry,full" if full?
     @bikes << bike
   end
+
   def release
       return "Sorry there are none" if empty?
       bikes.pop
   end
+
   private
-    def empty?
+  def full?
+    bikes.length >= CAPACITY
+  end
+
+  def empty?
     bikes.length == 0
-    end
+  end
 end
